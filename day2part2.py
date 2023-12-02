6b6a9efeb73632ad9e1c99f7ec642d8551d0bfa3
BugDevelopments <thetabeingness@gmail.com>
2023-12-02 18:30:09 +0100

    Update day2part2.py
    Refactored the max_red/max_blue/max_green lambda-functions as one lambda-functions

--- day2part2.py
+++ day2part2.py
@@ -8,12 +8,9 @@ def main():
   except FileNotFoundError:
     print(f"Error opening input file '{input_file}': FileNotFoundError")
     return
-
-  max_red = lambda g: max(map(int,re.findall(r"(\d+) red",g)))
-  max_blue = lambda g: max(map(int,re.findall(r"(\d+) blue",g)))
-  max_green = lambda g: max(map(int,re.findall(r"(\d+) green",g)))
   
-  print(sum(max_red(g)*max_blue(g)*max_green(g) for g in lines))
+  max_color = lambda c,g: max(map(int,re.findall(fr"(\d+) {c}",g))) # returns the maximal number of c-colored balls in game g
+  print(sum(max_color('red',g)*max_color('blue',g)*max_color('green',g) for g in lines))
 
 
 if __name__ == "__main__":

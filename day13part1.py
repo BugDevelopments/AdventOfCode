7cd526723e133f778f07d3468c2f3515a9f15d0f
BugDevelopments <thetabeingness@gmail.com>
2023-12-13 19:00:03 +0100

    Day 13

---
+++ day13part1.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def readInputFile(input_file):
+  try:
+    with open(input_file) as f:
+        return [l.strip() for l in f]
+  except FileNotFoundError:
+    print(f"Error opening input file '{input_file}': FileNotFoundError")
+    exit(-1)
+
+INPUT_FILE = 'input13.txt'
+
+def makeBinaryMatrix(M):
+  assert(all(len(M[0])==len(m) for m in M)) # all rows need to have equal length
+  return np.array([[mij=='#' for mij in mi] for mi in M])
+
+def isSymmetric(M,r,axis=0):
+  M = M.swapaxes(0,axis)
+  assert(0<r<M.shape[1])
+  m=min(M.shape[1]-r,r)
+  return (np.fliplr(M[:,r-m:r]) == M[:,r:r+m]).all()
+
+
+def getSymmetryNumber(M):
+  for r in range(1,M.shape[1]):
+    if isSymmetric(M,r):
+      return r
+  
+  for r in range(1,M.shape[0]):
+    if isSymmetric(M,r,axis=1):
+      return 100*r
+  
+lines = readInputFile(INPUT_FILE)
+I = [-1]
+I.extend([i for i,l in enumerate(lines) if l==''])
+I.extend([len(lines)])
+print(sum(getSymmetryNumber(makeBinaryMatrix(lines[i+1:j]))   for i,j in zip(I[0:], I[1:])))
